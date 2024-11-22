719ad5f3a5f4909454d2af89724bfa556be96cb7
ritacassis <163550065+ritacassis@users.noreply.github.com>
2024-11-22 11:25:08 -0300

    Add files via upload

---
+++ FlashcadsRita/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem descobriun o Brasil?',
+    'O Brasil não foi descoberto e sim envadido pelos estrangeiros'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é moscou'
+)
+
+criaCartao(
+    'Conhecimento gerais ',
+    'Como que faz vidro?',
+    'Vidro se faz com areia derretida'
+)
+
+criaCartao(
+    'Lingua Espanhola',
+    'Como se diz ônibus em espanhol?',
+    'Ônibus em espanhol é autobús'
+)
